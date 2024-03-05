4bb50490589c3fb41223f8af09ec916ebf3a1845
JUAN-MH <cmosqueraherrera1@gmail.com>
2024-03-05 13:54:38 -0500

    conexion a la base de datos

---
+++ Cine/src/utils/BaseDatos.java
@@ -0,0 +1,38 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import utils.Persona;
+
+public class BaseDatos {
+    
+    Connection conexion;
+    Statement manipularDB;
+    
+    public BaseDatos(){
+        String hostname = "localhost";
+        String puerto = "3306";
+        String databasename = "bd_cine";
+        String user = "root";
+        String password = "";
+        
+        String url = "jdbc:mysql://"+hostname+":"+puerto+"/"+databasename;
+        
+        try {
+            conexion = DriverManager.getConnection(url, user, password);
+            manipularDB = conexion.createStatement();
+            System.out.println("Conexion exitosa.");
+        } catch (SQLException ex) {
+            System.out.println("Error en conexion a BD:");
+            System.out.println(ex.getMessage());
+        }
+    }
+    
+    
+}
