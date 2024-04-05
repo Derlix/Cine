27bb7ca5e1f1722fddbf1b1b06dc3dd77a8feda2
Bustamante <sebasbga7@gmail.com>
2024-04-05 00:39:44 -0500

    update

---
+++ Cine/src/utils/BaseDatosJuanBustamante.java
@@ -0,0 +1,58 @@
+package utils;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BaseDatosJuanBustamante {
+    
+    Connection conexion;
+    Statement manipularDB;
+    
+    public BaseDatosJuanBustamante(){
+        String hostname = "localhost";
+        String puerto = "3306";
+        String databasename = "bd_cine";
+        String user = "root";
+        String password = "";
+        
+        String url = "jdbc:mysql://"+hostname+":"+puerto+"/"+databasename;
+        
+        try {
+            conexion = DriverManager.getConnection(url, user, password);
+            manipularDB = conexion.createStatement();
+            System.out.println("Conexion exitosa.");
+        } catch (SQLException ex) {
+            System.out.println("Error en conexion a BD:");
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public List<String> obtenerCines() {
+        List<String> cines = new ArrayList<>();
+        String query = "SELECT Nombre_Cine, Ciudad FROM cines";
+        try {
+            ResultSet rs = manipularDB.executeQuery(query);
+            while (rs.next()) {
+                cines.add(rs.getString("Nombre_Cine") + ", " + rs.getString("Ciudad"));
+            }
+        } catch (SQLException ex) {
+            System.out.println("Error al obtener los cines:");
+            System.out.println(ex.getMessage());
+        }
+        return cines;
+    }
+    
+    // En la clase BaseDatosJuanBustamante
+    public void actualizarSede(int idSede, String nombreSede, String direccion, String ciudad, String pais) {
+        String query = "UPDATE cine SET Nombre_Cine = '" + nombreSede + "', Direccion = '" + direccion + "', Ciudad = '" + ciudad + "', Pais = '" + pais + "' WHERE ID_Cine = " + idSede;
+        try {
+            manipularDB.executeUpdate(query);
+            System.out.println("Sede actualizada exitosamente.");
+        } catch (SQLException ex) {
+            System.out.println("Error al actualizar la sede:");
+            System.out.println(ex.getMessage());
+        }
+    }
+
+}
