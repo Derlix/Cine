4a95e1617a0d70ed5a4b1f2ecc6857e4d9a04a16
CristianArce2002 <131485610+CristianArce2002@users.noreply.github.com>
2024-04-05 15:54:20 -0500

    añadiendo objetos asiento y sala

--- Cine/src/utils/CristianBD.java
+++ Cine/src/utils/CristianBD.java
@@ -1,10 +1,63 @@
 
 package utils;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import utils.Asiento;
+import utils.Sala;
 
 
 public class CristianBD extends BaseDatosJuanPrincipal{
     
     
+    //Obtener asientos
+    public List<Asiento> obtenerAsientos() {
+        List<Asiento> asientos = new ArrayList<>();
+        try {
+            String query = "SELECT * FROM asientos";
+            PreparedStatement statement = conexion.prepareStatement(query);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                int id_asiento = resultSet.getInt("ID_Asiento");
+                int id_sala = resultSet.getInt("ID_Sala");
+                String fila = resultSet.getString("Fila");
+                int numero = resultSet.getInt("Numero");
+                String estado = resultSet.getString("Estado");
+                Asiento asiento = new Asiento(id_asiento, id_sala, fila, numero, estado);
+                asientos.add(asiento);
+            }
+        } catch (SQLException ex) {
+            System.out.println("Error al obtener los asientos:");
+            System.out.println(ex.getMessage());
+        }
+        return asientos;
+    }
+
+    //Obtener salas
+    public List<Sala> obtenerSalas() {
+        List<Sala> salas = new ArrayList<>();
+        try {
+            String query = "SELECT * FROM salas_cine";
+            PreparedStatement statement = conexion.prepareStatement(query);
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+
+                int id_sala = resultSet.getInt("ID_Sala");
+                String nombre = resultSet.getString("Nombre_Sala");
+                String capacidad = resultSet.getString("Capacidad");
+                int id_cine = resultSet.getInt("ID_Cine");
+                Sala sala = new Sala(id_sala, nombre, capacidad, id_cine);
+                salas.add(sala);
+            }
+        } catch (SQLException ex) {
+            System.out.println("Error al obtener las salas:");
+            System.out.println(ex.getMessage());
+        }
+        return salas;
+    }
+ 
 }
---
+++ Cine/src/utils/Sala.java
@@ -0,0 +1,50 @@
+
+package utils;
+
+public class Sala {
+    
+    private int id_sala;
+    private String nombre;
+    private String capacidad;
+    private int id_cine;
+
+    public Sala(int id_sala, String nombre, String capacidad, int id_cine) {
+        this.id_sala = id_sala;
+        this.nombre = nombre;
+        this.capacidad = capacidad;
+        this.id_cine = id_cine;
+    }
+
+    public int getId_sala() {
+        return id_sala;
+    }
+
+    public void setId_sala(int id_sala) {
+        this.id_sala = id_sala;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(String capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public int getId_cine() {
+        return id_cine;
+    }
+
+    public void setId_cine(int id_cine) {
+        this.id_cine = id_cine;
+    }
+    
+}
